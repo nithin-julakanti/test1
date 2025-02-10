478be74431dcc0657e8386d82b6c778487785fd6
nithin <nithin.@gmail.com>
2025-02-10 13:55:03 +0530

    moving the dags

--- test1.sh
+++ test1.sh
@@ -5,26 +5,19 @@ if [ ! -d ".git" ]; then
 	git init
 fi
 
-
-current_branch=$(git branch)
-
+#current_branch=$(git branch)
 
 git branch -M main
 
 
 git remote | grep -q origin || git remote add origin https://github.com/nithin-julakanti/test1.git
 
+git pull origin main
+
 git status
 git add .
 git commit -m "moving the dags"
 git push -u origin main
 
-#git_status=$(git status)
 
-#if [[ $git_status != *"nothing to commit, working tree clean"* ]]; then
-
-    #git add .
-   # git commit -m "new dags and modified dags are committed"
-   # git push origin main
-#fi
 echo "Successfully, dags have been copied to GitHub"
